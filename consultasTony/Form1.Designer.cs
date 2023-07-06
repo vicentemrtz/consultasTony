9f4813fd5e024e5de253208803e00ad1b50b81b0
vicentemrtz <135665367+vicentemrtz@users.noreply.github.com>
2023-07-06 00:00:25 -0600

    Diseño de la interfaz

--- consultasTony/Form1.Designer.cs
+++ consultasTony/Form1.Designer.cs
@@ -28,12 +28,221 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            dataGridView1 = new DataGridView();
+            panel1 = new Panel();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            Consulta_Cliente = new Button();
+            button2 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            pictureBox1 = new PictureBox();
+            label4 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(81, 216);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(831, 404);
+            dataGridView1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            panel1.AccessibleRole = AccessibleRole.None;
+            panel1.BackColor = Color.FromArgb(192, 0, 0);
+            panel1.Controls.Add(textBox3);
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(81, 119);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(831, 100);
+            panel1.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(655, 49);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(162, 23);
+            textBox3.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(368, 49);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(162, 23);
+            textBox2.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(96, 49);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(162, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Microsoft New Tai Lue", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.ForeColor = SystemColors.Control;
+            label3.Location = new Point(548, 55);
+            label3.Name = "label3";
+            label3.Size = new Size(92, 17);
+            label3.TabIndex = 2;
+            label3.Text = "Razon social: ";
+            label3.Click += label3_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Microsoft New Tai Lue", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.ForeColor = SystemColors.Control;
+            label2.Location = new Point(305, 55);
+            label2.Name = "label2";
+            label2.Size = new Size(39, 17);
+            label2.TabIndex = 1;
+            label2.Text = "RFC: ";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft New Tai Lue", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = SystemColors.Control;
+            label1.Location = new Point(17, 55);
+            label1.Name = "label1";
+            label1.Size = new Size(61, 17);
+            label1.TabIndex = 0;
+            label1.Text = "Clientes:";
+            // 
+            // Consulta_Cliente
+            // 
+            Consulta_Cliente.Cursor = Cursors.Hand;
+            Consulta_Cliente.Font = new Font("Microsoft New Tai Lue", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            Consulta_Cliente.Location = new Point(98, 645);
+            Consulta_Cliente.Name = "Consulta_Cliente";
+            Consulta_Cliente.Size = new Size(75, 23);
+            Consulta_Cliente.TabIndex = 2;
+            Consulta_Cliente.Text = "Consulta";
+            Consulta_Cliente.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Cursor = Cursors.Hand;
+            button2.Font = new Font("Microsoft New Tai Lue", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.Location = new Point(294, 646);
+            button2.Name = "button2";
+            button2.Size = new Size(131, 23);
+            button2.TabIndex = 3;
+            button2.Text = "Datos adicionales";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(823, 645);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 5;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Cursor = Cursors.Hand;
+            button5.Font = new Font("Microsoft New Tai Lue", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            button5.Location = new Point(551, 645);
+            button5.Name = "button5";
+            button5.Size = new Size(136, 23);
+            button5.TabIndex = 4;
+            button5.Text = "Limpiar pantalla";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Cursor = Cursors.Hand;
+            button6.Font = new Font("Microsoft New Tai Lue", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            button6.Location = new Point(823, 645);
+            button6.Name = "button6";
+            button6.Size = new Size(75, 23);
+            button6.TabIndex = 5;
+            button6.Text = "Salir";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Cursor = Cursors.No;
+            pictureBox1.Image = Properties.Resources.logo_papeleriastony;
+            pictureBox1.Location = new Point(275, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(446, 66);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 7;
+            pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Microsoft New Tai Lue", 20.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(331, 81);
+            label4.Name = "label4";
+            label4.Size = new Size(325, 35);
+            label4.TabIndex = 8;
+            label4.Text = "CONSULTA DE CLIENTES";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.MenuHighlight;
+            ClientSize = new Size(986, 686);
+            Controls.Add(label4);
+            Controls.Add(pictureBox1);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button2);
+            Controls.Add(Consulta_Cliente);
+            Controls.Add(panel1);
+            Controls.Add(dataGridView1);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private DataGridView dataGridView1;
+        private Panel panel1;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private Button Consulta_Cliente;
+        private Button button2;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private PictureBox pictureBox1;
+        private Label label4;
     }
 }
--- consultasTony/Form1.cs
+++ consultasTony/Form1.cs
@@ -6,5 +6,10 @@ namespace consultasTony
         {
             InitializeComponent();
         }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
